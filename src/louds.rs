0ca91e229371cd2422dfcb16d3746e2698d308fc
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2024-12-29 21:42:41 +0100

    Enable serde and mem_dbg by default

--- src/louds.rs
+++ src/louds.rs
@@ -1,11 +1,6 @@
 mod louds_impl;
 
 use fid::{BitVector, FID};
-#[cfg(feature = "serde")]
-use serde::{Deserialize, Serialize};
-
-#[cfg(feature = "mem_dbg")]
-use mem_dbg::{MemDbg, MemSize};
 
 pub type BitLouds = Louds<BitVector>;
 
@@ -16,8 +11,8 @@ pub type BitLouds = Louds<BitVector>;
 /// In fact, _N_ (number of nodes in the tree) is designed to be limited to: _N < 2^64 / 2_, while each node is represented in 2bits in average.<br>
 /// It should be enough for almost all usecases since a binary data of length of _2^63_ consumes _2^20 = 1,048,576_ TB (terabytes), which is hard to handle by state-of-the-art computer architecture.
 #[derive(Clone, Debug)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-#[cfg_attr(feature = "mem_dbg", derive(MemDbg, MemSize))]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "mem_dbg", derive(mem_dbg::MemDbg, mem_dbg::MemSize))]
 pub struct Louds<T: FID = BitVector> {
     lbs: T,
 }
@@ -29,15 +24,15 @@ pub struct LoudsError {
 }
 
 #[derive(PartialEq, Eq, Debug, Clone, Copy)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-#[cfg_attr(feature = "mem_dbg", derive(MemDbg, MemSize))]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "mem_dbg", derive(mem_dbg::MemDbg, mem_dbg::MemSize))]
 #[repr(transparent)]
 /// Node number of [Louds](struct.Louds.html) tree
 pub struct LoudsNodeNum(pub u64);
 
 #[derive(PartialEq, Eq, Debug, Clone, Copy)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-#[cfg_attr(feature = "mem_dbg", derive(MemDbg, MemSize))]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "mem_dbg", derive(mem_dbg::MemDbg, mem_dbg::MemSize))]
 #[repr(transparent)]
 /// Index of [Louds](struct.Louds.html) tree
 pub struct LoudsIndex(pub u64);
